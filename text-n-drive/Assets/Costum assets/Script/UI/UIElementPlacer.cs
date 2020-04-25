1f7a3edcff3345cdb42c921a408faec32ab296fc
Bernard Meunier <beurnii@gmail.com>
2020-04-25 11:41:18 -0400

    backup

---
+++ text-n-drive/Assets/Costum assets/Script/UI/UIElementPlacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class UIElementPlacer : MonoBehaviour
+{
+    public GameObject UIElement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 messagePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        UIElement.transform.position = messagePos;
+    }
+}
---
+++ text-n-drive/Assets/Costum assets/Script/UI/UIResizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UIResizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Resize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #if UNITY_EDITOR
+            Resize();
+        #endif
+    }
+
+    void Resize()
+    {
+        float posy = this.transform.position.y;
+        RectTransform rt = this.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(0, posy * 2);
+    }
+
+
+}
